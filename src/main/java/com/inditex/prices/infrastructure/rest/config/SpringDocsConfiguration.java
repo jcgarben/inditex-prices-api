26a73e49b6b3b9852f6041b4194da3d29048f3a6
Juan Carlos García <jcgarciabenitez@gmail.com>
2025-09-29 09:40:44 +0200

    feat(swagger): add swagger config

---
+++ src/main/java/com/inditex/prices/infrastructure/rest/config/SpringDocsConfiguration.java
@@ -0,0 +1,35 @@
+package com.inditex.prices.infrastructure.rest.config;
+
+import org.springdoc.core.configuration.SpringDocConfiguration;
+import org.springdoc.core.configuration.SpringDocUIConfiguration;
+import org.springdoc.core.properties.SpringDocConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration
+public class SpringDocsConfiguration {
+
+    @Bean
+    SpringDocConfiguration springDocConfiguration() {
+        return new SpringDocConfiguration();
+    }
+
+    @Bean
+    SpringDocConfigProperties springDocConfigProperties() {
+        return new SpringDocConfigProperties();
+    }
+
+    @Bean
+    ObjectMapperProvider objectMapperProvider(SpringDocConfigProperties springDocConfigProperties) {
+        return new ObjectMapperProvider(springDocConfigProperties);
+    }
+
+    @Bean
+    SpringDocUIConfiguration springDocUIConfiguration(Optional<SwaggerUiConfigProperties> optionalSwaggerUiConfigProperties) {
+        return new SpringDocUIConfiguration(optionalSwaggerUiConfigProperties);
+    }
+}
